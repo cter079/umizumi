35880b19851f37507b143925e93ab173956f3e32
Cter079 <cterlouw04@gmail.com>
2023-01-29 02:00:46 +0100

    connection string removal

--- index.js
+++ index.js
@@ -90,7 +90,7 @@ client.on ('ready', () => {
 
     client.user.setAvatar('https://i.ytimg.com/vi/4QoM-bnASnc/maxresdefault.jpg');
 
-mongoose.connect(`mongodb+srv://cter079:cgQUwruoPqwQnvy1@cluster0.eudhufn.mongodb.net/test`, {
+mongoose.connect(`${process.env.CONNECTION_STRING}`, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 }).then(() => {
